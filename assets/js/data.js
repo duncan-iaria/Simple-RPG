1b495179d7c8a4748036ca29df8100190a2f546f
Snoodleking <snoodleking@gmail.com>
2017-05-22 00:29:38 -0400

    basic fundamentals of the game are in - need to finish resolution

--- assets/js/data.js
+++ assets/js/data.js
@@ -13,7 +13,67 @@ function Pawn( tId, tName, tHealth, tBaseAttackPower, tCounterAttackPower )
 
     //current attack power starts as the base
     this.currentAttackPower = this.baseAttackPower;
-}
+
+    this.view = 
+	`<div id="pawn${this.id}" class="unit-container">
+		<img class="unit-image" src="assets/images/${this.id}.jpg"/>
+		<div class="unit-stats-container">
+			<div id="health${this.id}" class="unit-health"><p>${this.health}</p></div>
+		</div>
+		<div class="unit-name">${this.name}</div>
+	</div>`
+
+    //actions
+    this.spawn = function( tTarget )
+    {
+    	tTarget.append( this.view );
+
+    	//add a data attribute to this pawns html
+    	$( `#pawn${this.id}` ).attr( 'data-id', this.id );
+    },
+
+    this.onSelected = function()
+    {
+    	console.log( "hello I'm " + this.name + ", nice to mee you!" );
+    },
+
+    this.takeDamage = function( tAmt )
+    {
+        this.health -= tAmt;
+        this.updateHealthView();
+    },
+
+    //for when the player attacks, increment the player's damage
+    this.attack = function()
+    {
+        this.currentAttackPower += this.baseAttackPower;
+    },
+
+    this.onAttacked = function( tDamage )
+    {
+    	this.health -= tDamage;
+
+    	if( this.health > 0 )
+		{
+			//this.onCounterAttactk()
+		}
+        else
+        {
+            this.onDeath();
+        }
+    },
+
+    //update the health value of this pawn
+    this.updateHealthView = function()
+    {
+        $( `#health${this.id}` ).text( this.health.toString() );
+    },
+
+    this.onDeath = function()
+    {
+
+    }
+};
 
 //holds all the data for the game
 var data =
@@ -22,10 +82,12 @@ var data =
 
 	jarJar: new Pawn( 0, "Jar Jar", 100, 20, 50 ),
 	greedo: new Pawn( 1, "Greedo", 50, 10, 10 ),
-	hanSolo: new Pawn( 2, "Han Solo", 75, 15, 15),
+	hanSolo: new Pawn( 2, "Han Solo", 75, 15, 15 ),
 
 	init: function()
 	{
 		this.pawns = [ this.jarJar, this.greedo, this.hanSolo ];
 	}
 }
+
+data.init();
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,153 @@
-console.log("game hook");
+//TODO: SPLICING THE ARRAYS IS MESSING UP THE IDs
+//MAKE NEW ARRAY FOR GAME PLAY - BASE LOGIC OFF OF MASTER ARRAY
+//start the game after everything on the page has loaded
+$( window ).on( 'load', function()
+{
+	//setup game
+	rpgGame.init( data );
+	rpgGame.setViews( $( '#character-bank' ), $( '#current-player' ), $( '#current-enemy' ), $( '#enemy-bank' ) );
+
+	//attatch the battle button
+	$( '#attack-button' ).on( 'click', rpgGame.battle.bind( rpgGame ) );
+});
+
+//enums for keeping track of game state
+var stateEnum =
+{
+	PLAYERSELECT: 1,
+	OPPONENTSELECT: 2,
+	BATTLE: 3,
+};
+
+var rpgGame =
+{
+	//game variables
+	currentState: stateEnum.PLAYERSELECT,
+	currentPlayer: null,
+	currentOpponent: null,
+
+	//view variables
+	characterBankView: null,
+	currentPlayerView: null,
+	currentOpponentView: null,
+	enemyBankView: null,
+
+	init: function( tData )
+	{	
+		//save a reference to the character bank for spawn purposes
+		tempSpawnArea = $( "#character-bank" );
+
+		//spawn each of the pawns (for selection purpose)
+		$.each( tData.pawns, function( tIndex, tValue )
+		{	
+			tData.pawns[ tIndex ].spawn( tempSpawnArea );
+		});
+
+		//attach events
+		$( '.unit-container' ).on( 'click', function()
+		{
+			rpgGame.onPawnClicked( this );
+		});
+	},
+
+	setViews: function( tCharacterBankView, tCurrentPlayerView, tCurrentOpponentView, tEnemyBankView )
+	{
+		this.characterBankView = tCharacterBankView;
+		this.currentPlayerView = tCurrentPlayerView;
+		this.currentOpponentView = tCurrentOpponentView;
+		this.enemyBankView = tEnemyBankView;
+	},
+
+	onPawnClicked: function( tPawn )
+	{
+		switch( this.currentState )
+		{
+			case stateEnum.PLAYERSELECT:
+				this.setCurrentPlayer( tPawn );
+				break;
+
+			case stateEnum.OPPONENTSELECT:
+				this.setCurrentOpponent( tPawn );
+				break;
+
+			default:
+				break;
+		}
+	},
+
+	setCurrentPlayer: function( tPawn )
+	{
+		//set the game's current player to the one from the data
+		this.currentPlayer = data.pawns[ $( tPawn ).attr( "data-id" ) ];
+		this.currentState = stateEnum.OPPONENTSELECT;
+
+		//move all the pawns to the appropriate places
+		this.enemyBankView.append( $( '.unit-container' ) );
+
+		//move the selected one back (TODO: refactor this out?) 
+		this.currentPlayerView.append( $( `#pawn${this.currentPlayer.id}` ) );
+
+		this.removeCharacterFromPawnList( this.currentPlayer );
+	},
+
+	setCurrentOpponent: function( tPawn )
+	{
+		this.currentOpponent = data.pawns[ $(tPawn).attr( "data-id" ) ];
+		this.currentState = stateEnum.BATTLE;
+
+		//move the selected enemy into the battle zone
+		this.currentOpponentView.append( $( `#pawn${this.currentOpponent.id}` ) );
+	},
+
+	// BATTLE
+	battle: function()
+	{
+		//if we're in battle mode - then fight!
+		console.log( this.currentState );
+
+		if( this.currentState == stateEnum.BATTLE )
+		{
+			console.log( "we battlin now" );
+			this.currentOpponent.takeDamage( this.currentPlayer.currentAttackPower );
+
+			//enemy is still alive
+			if( this.currentOpponent.health > 0 )
+			{
+				this.currentPlayer.takeDamage( this.currentOpponent.counterAttackPower );
+
+			}
+			//enemy is dead
+			else
+			{
+				//remove the current enemy and set game back to enemy select
+				this.currentOpponentView.empty();
+				removeCharacterFromPawnList( this.currentOpponent );
+
+				//remove the pawn from the game list
+				//this.currentState = O
+			}
+		}
+
+		console.log( "battle!" );
+	},
+
+	removeCharacterFromPawnList: function( tPawn )
+	{
+		var tempIndex = data.pawns.indexOf( tPawn );
+
+		if( tempIndex > - 1 )
+		{
+			data.pawns.splice( tempIndex, 1 );
+			console.log( data.pawns );
+		}
+	},
+
+	//remove all the characters from the board
+	clearPawns: function()
+	{
+		this.characterBankView.empty();
+		this.currentPlayerView.empty();
+		this.currentOpponentView.empty();
+		this.enemyBankView.empty();
+	},
+};
